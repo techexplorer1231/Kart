fc982168e30ed8bfbc0cb1a6c0696872a9ce8b1a
dreamweaver1231 <dreamweaver1231@gmail.com>
2014-06-27 00:41:21 +0530

    Update config.js

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'ekart';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'angular-loading-bar', 'angular-carousel'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'angular-loading-bar'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName) {
